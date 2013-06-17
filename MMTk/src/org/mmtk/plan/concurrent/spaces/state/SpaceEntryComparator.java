afb6b83e343416966fabdb9e5518ede3a8b204ac
Ovidiu Maja <ovi_dan89@yahoo.com>
2013-06-17 20:57:57 +0300

    Added some explications for space comparator.

--- MMTk/src/org/mmtk/plan/concurrent/spaces/state/SpaceEntryComparator.java
+++ MMTk/src/org/mmtk/plan/concurrent/spaces/state/SpaceEntryComparator.java
@@ -50,6 +50,17 @@ public class SpaceEntryComparator<T extends Space> implements Comparator<Entry<T
         if (result == 0) {
             final long toCount = countBySpace.get(toEntry.getKey()).get();
             final long fromCount = countBySpace.get(fromEntry.getKey()).get();
+            /*
+             * M : return the space with the highest count
+             *      - represents the space used the most number of time as a TO_SPACE
+             *      - has the most chances to find some garbage objects
+             *      - toCount - fromCount => sorts the spaces by highest count first
+             *      
+             * C: return the space with the lowest count
+             *      - represents the space used the most number of time as a FROM_SPACE
+             *      - has the most chances to be mostly empty or or have a small amount of objects
+             *      - fromCount - toCount => sorts the spaces by lowest count first
+             * */
             return (int) (state == SpaceState.FROM_SPACE ? toCount - fromCount : fromCount - toCount);
         } else {
             return result;
